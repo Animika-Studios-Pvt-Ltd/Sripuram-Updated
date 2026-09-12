ef0dc14e2a3acb9af5b9b82b00d4067f0021a000
Asha <india.lumos@gmail.com>
2026-09-12 13:09:18 +0530

    publications

--- static/sripuram-org/custom.js
+++ static/sripuram-org/custom.js
@@ -111,42 +111,7 @@ $(window).on("resize", function () {
           $(e.target).prev(".panel-heading").removeClass("active");
         }));
   }));
-$(".cwd-volumes-slider").slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  loop: true,
-  autoplay: true,
-  speed: 1000,
-  autoplaySpeed: 3000,
-  arrows: true,
-  nextArrow:
-    '<div class="slick-custom-arrow slick-custom-arrow-right"><img src="/static/sripuram-org/arrow.webp" alt="Arrow" class="img-fluid"></div>',
-  prevArrow:
-    '<div class="slick-custom-arrow slick-custom-arrow-left"><img src="/static/sripuram-org/arrow.webp" alt="Arrow" class="img-fluid"></div>',
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
+
 if (typeof AOS !== "undefined") {
   AOS.refresh();
 }
